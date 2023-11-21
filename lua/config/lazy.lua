f85c763d1c59415773a6886d05a8506df2a8f562
Jinwen.Tong <tongjinwen1020@gmail.com>
2023-11-21 19:52:06 +0800

    modify config and add plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,11 +9,97 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        colorscheme = "solarized-osaka",
+        news = {
+          lazyvim = true,
+          neovim = true,
+        },
+      },
+    },
+
+    -- flutter language
+    {
+      "akinsho/flutter-tools.nvim",
+      lazy = false,
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "stevearc/dressing.nvim", -- optional for vim.ui.select
+      },
+      config = function()
+        require("flutter-tools").setup({})
+      end,
+    },
+
+    -- bufferline optimizing tabs
+    {
+      "akinsho/bufferline.nvim",
+      event = "VeryLazy",
+      keys = {
+        { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+        { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+      },
+      opts = {
+        options = {
+          mode = "tabs",
+          show_buffer_close_icons = false,
+          shwo_close_icon = false,
+        },
+      },
+    },
+
+    -- tabs optimizing show filename
+    {
+      "b0o/incline.nvim",
+      dependencies = { "craftzdog/solarized-osaka.nvim" },
+      event = "BufReadPre",
+      priority = 1200,
+      config = function()
+        local colors = require("solarized-osaka.colors").setup()
+        require("incline").setup({
+          highlight = {
+            groups = {
+              InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+              InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+            },
+          },
+          window = { margin = { vertical = 0, horizontal = 1 } },
+          hide = {
+            cursorline = true,
+          },
+          render = function(props)
+            local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+            if vim.bo[props.buf].modified then
+              filename = "[+] " .. filename
+            end
+
+            local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+            return { { icon, guifg = color }, { " " }, { filename } }
+          end,
+        })
+      end,
+    },
+
+    -- Incremental rename
+    {
+      "smjonas/inc-rename.nvim",
+      cmd = "IncRename",
+      config = true,
+    },
+
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
